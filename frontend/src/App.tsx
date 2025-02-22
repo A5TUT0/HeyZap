02fcb5e6a12634ed7fc744a19e1cd1d74df46903
A5TUT0 <noahlezama2000@gmail.com>
2025-02-22 12:30:17 +0100

    feat: implement routing with React Router; add registration route and restructure App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,27 @@
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Chat from "./components/Chat";
 import ListOfUserActive from "./components/ListOfUserActive";
+import Register from "./components/Register";
 
 export default function App() {
   return (
-    <div className="h-screen flex items-center justify-center bg-gray-900">
-      <SignedOut>
-        <div className="flex items-center justify-center h-full">
-          <SignInButton />
-        </div>
-      </SignedOut>
-
-      <SignedIn>
-        <div className="absolute top-4 left-4">
-          <UserButton />
-        </div>
-        <div className="flex justify-center items-center h-full m-5">
-          <ListOfUserActive />
-        </div>
-        <Chat />
-      </SignedIn>
-    </div>
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div className="h-screen flex items-center justify-center bg-gray-900">
+              <div className="flex items-center justify-center h-full"></div>
+              <div className="absolute top-4 left-4"></div>
+              <div className="flex justify-center items-center h-full m-5">
+                <ListOfUserActive />
+              </div>
+              <Chat />
+            </div>
+          }
+        />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
   );
 }
